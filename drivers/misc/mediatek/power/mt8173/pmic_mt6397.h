5e2ac1ae62ce6d7dface8d9806f15dd713511e85
Anmin Hsu <anmin.hsu@mediatek.com>
2015-07-15 10:51:20 +0800

    [ALPS01812857] PMIC: Add IRQ
    [Detail]
    Add PMIC MT6397/MT6391 IRQ
    
    [Solution]
    1. Add PMIC irq chip.
    2. Register PMIC interrupt.
    
    [Feature]
    Power Management.
    
    MTK-Commit-Id: 8d595568c4355e3d9a3a4e20029926209e8005a4
    
    Change-Id: I80619130ab9271f94403e8a759bc8171ef44f70b
    CR-Id: ALPS01812857

---
+++ drivers/misc/mediatek/power/mt8173/pmic_mt6397.h
@@ -0,0 +1,39 @@
+#ifndef _PMIC_MT6397_H_
+#define _PMIC_MT6397_H_
+
+extern void kpd_pwrkey_pmic_handler(unsigned long pressed);
+extern void kpd_pmic_rstkey_handler(unsigned long pressed);
+
+struct mt6397_event_stat {
+	u64 last;
+	int count;
+	bool blocked:1;
+	bool wake_blocked:1;
+};
+
+struct mt6397_chip_priv {
+	struct device *dev;
+	struct irq_domain *domain;
+	unsigned long event_mask;
+	unsigned long wake_mask;
+	unsigned int saved_mask;
+	u16 int_con[2];
+	u16 int_stat[2];
+	int irq;
+	int irq_base;
+	int num_int;
+	int irq_hw_id;
+	bool suspended:1;
+	unsigned int wakeup_event;
+	struct mt6397_event_stat stat[32];
+};
+
+struct mt6397_irq_data {
+	const char *name;
+	unsigned int irq_id;
+	 irqreturn_t (*action_fn)(int irq, void *dev_id);
+	bool enabled:1;
+	bool wake_src:1;
+};
+
+#endif				/* _PMIC_MT6397_H_ */
