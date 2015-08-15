dd89a622368a2eda7445a3b3e0330e7755cd8c27
Anmin Hsu <anmin.hsu@mediatek.com>
2015-08-16 01:40:07 +0800

    [ALPS02161122] display : init the color format by config
    [Detail] fb0 init the color format by config
    [Solution] fb0 init the color format by config
    [Feature] fb0 init the color format by config
    
    MTK-Commit-Id: 4f44af7011f86d3b0030050c7a38dbaba29e80d1
    
    Change-Id: Iabaaec14a8c0f6e1ef0d4e6593ab1d76598a688c
    CR-Id: ALPS02161122
    CR-Title: [8173][display] modify code for kernel 3.18
    CR-Template: null
    CR-Source: RD
    CR-Class: Code Inspection
    Depend-Changelist:

--- drivers/misc/mediatek/video/mt8173/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt8173/videox/mtkfb.c
@@ -1683,7 +1683,7 @@ static int mtkfb_fbinfo_init(struct fb_info *info)
 
 	var.transp.offset = 24;
 	var.transp.length = 8;
-#if 1
+#ifdef CONFIG_MTK_GPU_SUPPORT
 	/* default as BGRA, GPU(default data is BGRA too) will not change this value at home screen */
 	var.red.offset = 16;
 	var.red.length = 8;
